6e99567ec8322522e6eef79ff8e812db45e86489
munteannatan <33986780+munteannatan@users.noreply.github.com>
2018-10-11 19:05:36 +0100

    Update app.js

--- app.js
+++ app.js
@@ -108,22 +108,22 @@ function handleMessage(sender_psid, received_message) {
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
     response = {
-      "payload": {
+      "attachment": {
         "type": "template",
         "payload": {
           "template_type": "generic",
           "elements": [{
-            "title": "Functionalities:",
-            "subtitle": "Make your choice.",
+            "title": "Is this the right picture?",
+            "subtitle": "Tap a button to answer.",
             "buttons": [
               {
                 "type": "postback",
-                "title": "name pending",
+                "title": "Yes!",
                 "payload": "yes",
               },
               {
                 "type": "postback",
-                "title": "name pending",
+                "title": "No!",
                 "payload": "no",
               }
             ],
