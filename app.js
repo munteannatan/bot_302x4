abe69a1014db04b1798184ba803588888dd84325
munteannatan <33986780+munteannatan@users.noreply.github.com>
2018-10-11 18:29:24 +0100

    Update app.js

--- app.js
+++ app.js
@@ -107,42 +107,39 @@ function handleMessage(sender_psid, received_message) {
   if (received_message.text) {    
     // Create the payload for a basic text message, which
     // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}".`
-    }
-     
+    if (received_message.text == "/help") {
+      response = {
+        "help_menu": {
+          "type": "template",
+          "payload": {
+            "template_type": "generic",
+            "elements": [{
+              "title": "Functions:",
+              "subtitle": "Make a choice",
+              "buttons": [
+                {
+                 "type": "postback",
+                  "title": "name pending",
+                  "payload": "yes",
+                },
+                {
+                  "type": "postback",
+                  "title": "name pending",
+                  "payload": "no",
+                }
+              ],
+            }]
+          }
+        }
+      }
+    } else if (received_message.text[0] == '/'){
+       response = {
+            "text": `You sent the message: "${received_message.text}".`
+       }
   
      // Send the response message
       callSendAPI(sender_psid, response);  
-  } //else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    //let attachment_url = received_message.attachments[0].payload.url;
-    //response = {
-      //"attachment": {
-       // "type": "template",
-       // "payload": {
-        //  "template_type": "generic",
-        //  "elements": [{
-         //   "title": "Is this the right picture?",
-         //   "subtitle": "Tap a button to answer.",
-         //   "image_url": attachment_url,
-         //   "buttons": [
-         //     {
-          //      "type": "postback",
-          //      "title": "Yes!",
-         //       "payload": "yes",
-         //     },
-          //    {
-         //       "type": "postback",
-         //       "title": "No!",
-         //       "payload": "no",
-          //    }
-         //   ],
-        //  }]
-       // }
-     // }
-   // }
- // }  
+  }  
 }
 
 function handlePostback(sender_psid, received_postback) {
